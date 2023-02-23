431535a63b47560c4b58652135a09bed19c3dfa1
Mayuri Mahto <mahtomayuri@gmail.com>
2023-02-23 17:19:16 +0530

    CH-2

---
+++ CH-2/index.js
@@ -0,0 +1,35 @@
+//Arithmetic Operators
+let a=10;
+let b= 3;
+console.log("a + b =", a+b)
+console.log("a - b =", a-b)
+console.log("a / b =", a/b)
+console.log("a * b =", a*b)
+console.log("a ** b =", a**b)
+console.log("a % b =", a%b)
+console.log("++a =", ++a)
+console.log("a++ =", a++)
+console.log("--a =", --a)
+console.log("a-- =", a--)
+console.log("a =", a)
+console.log("a-- =", a--)
+
+//Assignment Operators
+let x=5;
+x+=20;
+console.log(x)
+
+//Comparison Operators
+let comp1 = 6;
+let comp2 = "6";
+console.log("comp1==comp2", comp1==comp2)
+console.log("comp1!=comp2", comp1!=comp2)
+console.log("comp1===comp2", comp1===comp2)
+console.log("comp1!==comp2", comp1!==comp2)
+
+//Logical Operators
+let m = 5;
+let n = 6;
+console.log(m<n && m==5)
+console.log(m>n || m==5)
+console.log(!false)
