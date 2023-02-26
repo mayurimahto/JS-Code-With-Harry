686483b790acf657628560acc4ce329bfd596e0f
Mayuri Mahto <mahtomayuri@gmail.com>
2023-02-26 15:10:01 +0530

    ch-5

--- CH-5/index.js
+++ CH-5/index.js
@@ -43,3 +43,37 @@ console.log(num)
 
 let e = num.unshift(6)
 console.log(num)
+
+delete num[0]
+console.log(num)  // does not change the length
+
+let num2 = [7, 8, 9, 10]
+let num3 = [ 11, 12, 13, 14, 15]
+let newarray = num.concat(num2, num3)
+console.log(newarray)
+
+let num4 = [55, 3, 35, 4, 7, 9, 789, 54, 5, 40, 1]
+console.log(num4.sort()) // sorts alphabetically
+
+//for ascending order sorting
+let compare = (a, b)=> {
+    return a-b  // positive hoga toh ascending
+}
+num4.sort(compare)
+
+console.log(num4)
+
+// for ddescending order sorting
+let compare2 = (a, b)=> {
+    return b-a
+}
+num4.sort(compare2)
+console.log(num4)
+
+num.reverse()
+console.log(num)
+
+//splice and slice //vey important
+
+num.splice(1, 2, 1011, 1022, 1033, 1044, 1055)
+console.log(num)
